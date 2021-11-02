fe91de1dc03dd5a76f9290ec371aa808180c59fc
moukrime <moukrimeabdelhaq@gmail.com>
2021-11-02 22:59:56 +0100

    init rn template

---
+++ template.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Placeholder used to rename and replace in files
+  // package.json, index.json, android/, ios/
+  placeholderName: "AwesomeProject",
+
+  // Directory with template
+  templateDir: "./template",
+};
